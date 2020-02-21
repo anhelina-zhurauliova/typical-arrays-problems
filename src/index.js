75ff67fa48b0a25c06d5064d997db27e36618f81
Anhelina Zhurauliova <anhelinazhurauliova@gmail.com>
2020-02-21 20:23:43 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length ? array.sort(function(a, b) { return a - b;})[0] : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length ? array.sort(function(a, b) { return b - a; })[0] : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  function sumArray(array){
+    var sum = 0;
+    for (let i = 0; i < array.length; i++){
+      sum += array[i];
+    }
+    return sum;
+  }
+  return array && array.length ? sumArray(array) / array.length : 0;
 }
